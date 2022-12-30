c4ecb0c3e7e4e255b0ba5892f7927285ce43548a
m.alfan <114430354+pastibisa222@users.noreply.github.com>
2022-12-30 15:49:06 +0700

    Add files via upload

---
+++ UAS_NO2_INHERITANCE/src/venom.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Windows 10 Pro
+ */
+public class venom extends villain{
+    
+    //variabel
+    String jump="melompat";
+    
+    //fungsi
+    void jump(){
+        System.out.println("Venom bergerak dengan cara = "+jump);
+    }
+}
